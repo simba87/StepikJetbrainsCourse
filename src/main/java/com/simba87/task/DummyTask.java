26f88dacca57595acd519d43f9312539af7a16a9
simba87 <simba87@users.noreply.github.com>
2019-07-17 02:04:15 +0300

    Dummy task added
    Dummy task and package structure

---
+++ src/main/java/com/simba87/task/DummyTask.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.simba87.task;
+
+/**
+ *
+ * @author simba87
+ */
+public class DummyTask {
+    public static void main(String[] args) {
+        System.out.println("Dummy task");
+    }
+}
